72af63565fa813d16e6b50693781d1bffb3dd37e
Lovro Cvitas <lcvitas@gmail.com>
2024-02-12 15:18:40 +0100

    Init

---
+++ Example2.py
@@ -0,0 +1,8 @@
+print ("Add")
+
+a = input("First number: ")
+a = int(a)  # Convet the string input to int
+b = input("Second number: ")
+b = int(b)  # Convet the string input to int
+sum = a + b
+print ("Sum: %s" % sum)
